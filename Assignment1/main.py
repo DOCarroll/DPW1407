8cf8a92c67e1e4d54993793302c63f4ede650b1b
Danny O'Carroll <LemonMyth@live.com>
2014-07-08 21:45:31 -0400

    Created Number Strings

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -10,3 +10,9 @@ Midlib
 first_name = raw_input('Enter your first Name: ')
 last_name = raw_input('Enter your last Name: ')
 gender = raw_input('Enter your gender, M or F: ')
+
+#Create the number variables
+
+age = raw_input('Enter your age: ')
+web_experience = raw_input('Enter how many years have you been working with web development: ')
+school = raw_input('Enter the number of years you have been in school for web: ')
