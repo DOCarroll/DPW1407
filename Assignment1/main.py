af9e8ddc1c09ff9666deefeff02100d347493e07
Danny O'Carroll <LemonMyth@live.com>
2014-07-08 21:43:31 -0400

    Create String Variables

---
+++ Assignment1/main.py
@@ -0,0 +1,12 @@
+'''
+Daniel O'Carroll
+1407 DPW
+Midlib
+'''
+
+
+#Create the String variables
+
+first_name = raw_input('Enter your first Name: ')
+last_name = raw_input('Enter your last Name: ')
+gender = raw_input('Enter your gender, M or F: ')
---
+++ IntroToPython/main.py
@@ -0,0 +1,9 @@
+'''
+Danny O'Carroll
+1407 DPW
+Intro Stuff
+'''
+
+
+age = raw_input("Type your age: ")
+print "You are " + age + " years old."
