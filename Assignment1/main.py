983a61591fb3bbd62424b12627ccf626f1c25205
Danny O'Carroll <LemonMyth@live.com>
2014-07-08 22:10:03 -0400

    Added a mathematical operator, and an assignment

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -9,11 +9,12 @@ Midlib
 first_name = raw_input('Enter your first Name: ')
 last_name = raw_input('Enter your last Name: ')
 gender = raw_input('Enter your gender, M or F: ')
+your_life = ' '
 
 #Create the number variables
 age = raw_input('Enter your age: ')
 web_experience = raw_input('Enter how many years have you been working with web development: ')
-school = raw_input('Enter the number of years you have been in school for web: ')
+school = raw_input('Enter the number of years you have been in college: ')
 
 #create an array
 positions = ['Junior Software Engineer', 'Senior Software Engineer', 'CEO']
@@ -26,6 +27,15 @@ phones = {
     "ceo": 'iPhone 5s'
 }
 
+#create functions
+
+
+def experience(age, school):
+    life = age/school
+    if life >= 3:
+        your_life = 'I am sorry your life sucks'
+    elif life < 3:
+        your_life = 'Looks like you took care of business!'
 
 
 for position in positions:
