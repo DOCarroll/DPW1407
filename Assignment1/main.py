c41b088dfe1a8cead403418a182e5a1539b9a93b
Danny O'Carroll <LemonMyth@live.com>
2014-07-08 21:49:21 -0400

    Created an Array

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -6,13 +6,15 @@ Midlib
 
 
 #Create the String variables
-
 first_name = raw_input('Enter your first Name: ')
 last_name = raw_input('Enter your last Name: ')
 gender = raw_input('Enter your gender, M or F: ')
 
 #Create the number variables
-
 age = raw_input('Enter your age: ')
 web_experience = raw_input('Enter how many years have you been working with web development: ')
 school = raw_input('Enter the number of years you have been in school for web: ')
+
+#create an array
+positions = ['Junior Software Engineer', 'Senior Software Engineer', 'CEO']
+
